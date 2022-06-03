a845b6cafa98b21fd1e79384fcbbf15618d45777
villainsff7 <villainsff790@gmail.com>
2022-06-03 20:28:25 +0900

    Java_practice

---
+++ 16　カプセル化/StuSample5.java
@@ -0,0 +1,12 @@
+class StuSample5 {
+    public static void main (String[] args){
+        Student5 stu1 = new Student5("›Œ´");
+        stu1.setScore(80);
+        stu1.display();
+
+        Student5 stu2 = new Student5("‘ºR");
+        stu2.setScore(-50);
+        //stu2.score = -50;
+        stu2.display();
+    }
+}
---
+++ 16　カプセル化/Student5.java
@@ -0,0 +1,20 @@
+public class Student5 {
+    private String name;
+    private int score;
+    //privateを設定すれば大人数の開発でも思い通りにシステムが作れる。
+
+    public Student5(String n) {
+        name = n;
+    }
+
+    public void setScore(int s) {//値が正しいか確認する。
+        if (0 <= s && s <= 100) {
+            score = s;
+        } else {
+            System.out.println(name + "さんの点数が範囲外です");
+        }
+    }
+    void display() {
+        System.out.println(name + "さん:" + score + "点");
+    }
+}
