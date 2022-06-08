b340ffb10e4e0c9dc95d201249def09e0ac54aa2
villainsff7 <villainsff790@gmail.com>
2022-06-08 15:15:49 +0900

    Java_practice

---
+++ 演習問題５/Taxi2.java
@@ -0,0 +1,25 @@
+public class Taxi2 extends Car2 {
+    private int price;
+
+    public Taxi2(int no) {
+        this(no, 0);
+    }
+
+    public Taxi2(int no, int price) {
+        super(no);
+        this.price = price;
+    }
+
+    public void start() {
+        price = 420;
+    }
+
+    public void run() {
+        price = price + 80;
+    }
+
+    public void display() {
+        super.display();
+        System.out.println("—¿‹à‚Í" +price + "‰~‚Å‚·");
+    }
+}
