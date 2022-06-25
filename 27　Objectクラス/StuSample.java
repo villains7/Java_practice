e7c895785550bc8802aa4fd1b719ab7d5d647294
坂田千啓 <villainsff790@gmail.com>
2022-06-25 09:55:46 +0900

    Java_practice

--- 27　Objectクラス/StuSample.java
+++ 27　Objectクラス/StuSample.java
@@ -3,7 +3,7 @@ public class StuSample{
         Student stu1 = new Student("佐野");
         Student stu2 = new Student("佐野");
 
-        boolean result = stu1.equals(stu2);
+        boolean result = stu1.equals(stu2);//equalメソッドはインスタンスの住所が同じかどうか比べている。
         System.out.println(result);
         System.out.println(stu1);
     }
